b0c49442aa45ca118b36352f7f641fd0392b3abe
Brett Raper <72267696+techforchrist5227@users.noreply.github.com>
2023-08-08 06:26:16 -0400

    Task react project commit.  Update of react files

---
+++ taskopedia/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Course</h1>
+        <p>We will learn react in this course</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
