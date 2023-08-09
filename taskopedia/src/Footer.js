2b5942d6dbb42de03e649c23491796988099918a
Brett Raper <72267696+techforchrist5227@users.noreply.github.com>
2023-08-09 06:58:36 -0400

    updated files to jsx and added the enrolled students section

--- taskopedia/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <footer > <p style={{color:"white",backgroundColor:"black"}}>Happy Coding</p> </footer>
-  )
-}
-
-export default Footer
--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const submitButton = document.getElementById("submitButton");
+  const inputContainer = document.getElementById("inputContainer");
+  let inputCount = 1;
+
+  submitButton.addEventListener("click", function() {
+      const inputElement = document.createElement("input");
+      inputElement.setAttribute("type", "text");
+      inputElement.setAttribute("name", "input" + inputCount);
+      inputElement.setAttribute("placeholder", "Input " + inputCount);
+      inputContainer.appendChild(inputElement);
+      
+      inputCount++;
+  });
+});
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
